55e6b1033061266dfdb89b941bb71d366de550f4
Matt Hoyle <matt@deployable.co>
2015-10-18 12:25:12 +0100

    docco updates

--- doc/assets/behavior.js
+++ doc/assets/behavior.js
@@ -165,7 +165,8 @@ f.event={add:function(a,c,d,e,g){var h,i,j,k,l,m,n,o,p,q,r,s;if(!(a.nodeType===3
         "pageTitle": "package",
         "title": "package"
       },
-      "depth": 1
+      "depth": 1,
+      "outline": []
     }, {
       "type": "folder",
       "data": {
